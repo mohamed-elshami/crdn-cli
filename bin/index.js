b2a618e2ad7babeba648ccff495985de431bf929
Mohamed Samir Elshami <mohamed.samir@creiden.com>
2026-03-17 15:21:18 +0200

    Update application name and improve spawn command formatting in index.js

--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,11 @@ function run(command, args, cwd) {
   return new Promise((resolve, reject) => {
     const isWin = process.platform === "win32";
     const child = isWin
-      ? spawn([command, ...args].join(" "), [], { cwd, stdio: "inherit", shell: true })
+      ? spawn([command, ...args].join(" "), [], {
+          cwd,
+          stdio: "inherit",
+          shell: true,
+        })
       : spawn(command, args, { cwd, stdio: "inherit" });
     child.on("error", reject);
     child.on("close", (code) => {
@@ -118,8 +122,8 @@ async function main(projectName, options) {
 const program = new Command();
 
 program
-  .name("create-crdn-app")
-  .description("Create a CRDN app from a template")
+  .name("create-motest-app")
+  .description("Create a motest app from a template")
   .argument(
     "[project-name]",
     "Folder name for the new project (you'll be prompted if omitted)",
